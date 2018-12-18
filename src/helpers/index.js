7b13093e53753b6d434de03b3ccab9d3bcf038aa
kostia <kl@anadeainc.com>
2018-12-19 01:19:23 +0200

    Deleted excess line

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -6,7 +6,6 @@ export function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-
 export const missingParams = (obj, attrs) => attrs.filter((el) => el !== IS_COLLECTION).filter((el) => !R.contains(el, R.keys(obj)))
 
 const isArray = (arg) => Object.prototype.toString.call(arg) === '[object Array]'
